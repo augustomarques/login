861c92fc84815efba3a68745c1d09aacb5fa2a21
Augusto Marques <augusto.marques12@gmail.com>
2023-07-29 16:39:49 -0300

    first commit

---
+++ src/test/java/br/com/amarques/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.amarques.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
