c3623e22c313983f443b9293d49230515fabd369
Augusto Marques <augusto.marques12@gmail.com>
2023-07-31 12:58:01 -0300

    improving api's documentation

--- src/main/java/br/com/amarques/login/resource/AuthenticationController.java
+++ src/main/java/br/com/amarques/login/resource/AuthenticationController.java
@@ -5,6 +5,8 @@ import br.com.amarques.login.dto.form.RefreshTokenForm;
 import br.com.amarques.login.dto.form.RegisterRequest;
 import br.com.amarques.login.dto.view.AuthenticationResponse;
 import br.com.amarques.login.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Register and Authentication")
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
@@ -21,16 +24,19 @@ public class AuthenticationController {
     private final AuthenticationService service;
 
     @PostMapping("/register")
+    @Operation(summary = "Register a new user")
     public ResponseEntity<AuthenticationResponse> register(@Valid @RequestBody RegisterRequest request) {
         return ResponseEntity.ok(service.register(request));
     }
 
     @PostMapping("/authenticate")
+    @Operation(summary = "Authenticates a user")
     public ResponseEntity<AuthenticationResponse> authenticate(@Valid @RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(service.authenticate(request));
     }
 
     @PostMapping("/refresh-token")
+    @Operation(summary = "Updates the tokens")
     public ResponseEntity<AuthenticationResponse> refreshToken(@Valid @RequestBody RefreshTokenForm refreshTokenForm) {
         return ResponseEntity.ok(service.refreshToken(refreshTokenForm));
     }
--- src/main/java/br/com/amarques/login/resource/ResourceTest.java
+++ src/main/java/br/com/amarques/login/resource/ResourceTest.java
@@ -1,15 +1,19 @@
 package br.com.amarques.login.resource;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Test authentication")
 @RequestMapping("/api/test")
 public class ResourceTest {
 
     @GetMapping
+    @Operation(summary = "Request for testing authentication")
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("You are in");
     }
