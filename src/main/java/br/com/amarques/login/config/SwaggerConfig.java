38c1e9288cf50067640db76b929c55d3c633c15b
Augusto Marques <augusto.marques12@gmail.com>
2023-07-31 12:40:05 -0300

    Configuring Swagger

--- src/main/java/br/com/amarques/login/config/SwaggerConfig.java
+++ src/main/java/br/com/amarques/login/config/SwaggerConfig.java
@@ -4,20 +4,40 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import io.swagger.v3.core.jackson.ModelResolver;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "https://login.amarques-dev.com"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
 @SecurityScheme(
         name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
         type = SecuritySchemeType.HTTP,
         bearerFormat = "JWT",
-        scheme = "bearer"
+        in = SecuritySchemeIn.HEADER
 )
-@OpenAPIDefinition(servers = @Server(url = "/", description = "Default Server URL"))
 public class SwaggerConfig {
 
     @Bean
